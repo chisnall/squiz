273c70ed68213793e0b98a6c6223f052568f9936
Lee Chisnall <lee.chiz.uk@gmail.com>
2025-10-27 07:55:04 +0000

    Add new title config option

--- config/squiz.php
+++ config/squiz.php
@@ -5,6 +5,6 @@ return [
     'token' => env('SQUIZ_TOKEN'),
     'polling_interval' => env('SQUIZ_POLLING_INTERVAL', 1000),
     'route_path' => env('SQUIZ_ROUTE_PATH', '/squiz'),
+    'title' => env('SQUIZ_TITLE', 'Squiz'),
     'heading' => env('SQUIZ_HEADING', 'Squiz'),
-
 ];
