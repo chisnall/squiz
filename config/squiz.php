9b597c29960a6a5c0895d715e2f168818f58952f
Lee Chisnall <lee.chiz.uk@gmail.com>
2025-10-29 22:39:42 +0000

    Squiz config

--- config/squiz.php
+++ config/squiz.php
@@ -4,7 +4,9 @@ return [
 
     'token' => env('SQUIZ_TOKEN'),
     'polling_interval' => env('SQUIZ_POLLING_INTERVAL', 1000),
+    'storage_path' => env('SQUIZ_STORAGE_PATH', storage_path()),
     'route_path' => env('SQUIZ_ROUTE_PATH', '/squiz'),
     'title' => env('SQUIZ_TITLE', 'Squiz'),
     'heading' => env('SQUIZ_HEADING', 'Squiz'),
+
 ];
