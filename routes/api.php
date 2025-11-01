2a4ad78ffb77ff8791482554063ca2b12c047b9b
Lee Chisnall <lee.chiz.uk@gmail.com>
2025-11-01 12:22:26 +0000

    API routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Chisnall\Squiz\Controllers\SquizController;
 use Illuminate\Support\Facades\Route;
 
-Route::post(config('squiz.route_path') . '/clear', [SquizController::class, 'clearLog']);
-Route::post(config('squiz.route_path') . '/delete', [SquizController::class, 'deleteEntry']);
-Route::post(config('squiz.route_path') . '/entries', [SquizController::class, 'getLogEntries']);
-Route::get(config('squiz.route_path') . '/ids', [SquizController::class, 'getLogIds']);
+Route::post(config('squiz.route_path') . '/clear', [SquizController::class, 'clearLog'])->name('squizClear');
+Route::post(config('squiz.route_path') . '/delete', [SquizController::class, 'deleteEntry'])->name('squizDelete');;
+Route::post(config('squiz.route_path') . '/entries', [SquizController::class, 'getLogEntries'])->name('squizEntries');
+Route::get(config('squiz.route_path') . '/ids', [SquizController::class, 'getLogIds'])->name('squizIds');
