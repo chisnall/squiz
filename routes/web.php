7a506b00a7d20eeba96ad020d5b2b86602dc481d
Lee Chisnall <lee.chiz.uk@gmail.com>
2025-11-01 12:22:35 +0000

    Web routes

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Chisnall\Squiz\Controllers\SquizController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('squiz.route_path'), [SquizController::class, 'index']);
+Route::get(config('squiz.route_path'), [SquizController::class, 'index'])->name('squizIndex');
